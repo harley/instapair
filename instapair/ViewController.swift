a85e9e216b9a21ff5736d55c2c7fe5372853e962
Harley Trung <harley@harley.io>
2015-08-27 18:56:57 +0700

    Initial Commit

---
+++ instapair/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instapair
+//
+//  Created by Harley Trung on 8/27/15.
+//  Copyright (c) 2015 ht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
